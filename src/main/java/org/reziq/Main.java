97ea7949088ed592c08f9a7b3337babc806a885a
Abdulrazak <rizeq@Abdulrazaks-MacBook-Pro.local>
2024-10-07 17:21:21 +0200

    integrating the multiple API with common Json response value

--- src/main/java/org/reziq/Main.java
+++ src/main/java/org/reziq/Main.java
@@ -22,6 +22,32 @@ public class Main {
         String placeID = jsonPath.getString("place_id");
 
         System.out.println(placeID);
+        System.out.println();
+
+        // updating place
+
+        given().log().all().queryParam("key", "qaclick123")
+                .header("Content-Type", "application/json")
+                .body("{\n" +
+                        "\"place_id\":\"" +placeID+ "\",\n" +
+                        "\"address\":\"70 Summer walk, USA\",\n" +
+                        "\"key\":\"qaclick123\"\n" +
+                        "}").when().put("/maps/api/place/update/json")
+                .then().assertThat().log().all().statusCode(200).body("msg",
+                        equalTo("Address successfully updated"));
 
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
