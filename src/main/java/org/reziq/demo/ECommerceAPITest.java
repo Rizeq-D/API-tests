abbcc8007803a984de381ba05cbf24c8c8d11544
Abdulrazak <rizeq@Abdulrazaks-MacBook-Pro.local>
2024-10-23 15:47:06 +0200

    automate post calls which has form parameters and attachments using Rest Assured

--- src/main/java/org/reziq/demo/ECommerceAPITest.java
+++ src/main/java/org/reziq/demo/ECommerceAPITest.java
@@ -2,7 +2,10 @@ package org.reziq.demo;
 
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
 import io.restassured.specification.RequestSpecification;
+
+import java.io.File;
 
 import static io.restassured.RestAssured.given;
 
@@ -27,5 +30,53 @@ public class ECommerceAPITest {
         System.out.println(loginResponse.getToken());
         System.out.println(loginResponse.getUserId());
         System.out.println(loginResponse.getMessage());
+
+        String token = loginResponse.getToken();
+        String userId = loginResponse.getUserId();
+
+        // Add a product to the fictional e-commerce.
+        RequestSpecification addProductBaseRequest =
+                new RequestSpecBuilder().setBaseUri("https://rahulshettyacademy.com")
+                        .addHeader("authorization", token).build();
+
+        RequestSpecification requestAddProduct = given().log().all().spec(addProductBaseRequest)
+                .param("productName", "Bike")
+                .param("productAddedBy", userId)
+                .param("productCategory", "fashion")
+                .param("productSubCategory", "shirts")
+                .param("productPrice", "500")
+                .param("productDescription", "Lenova")
+                .param("productFor", "men");
+
+        String addProductResponse = requestAddProduct.when()
+                .post("/api/ecom/product/add-product")
+                .then().log().all().extract().response().asString();
+        JsonPath jsonPath = new JsonPath(addProductResponse);
+        String productId = jsonPath.getString("productId");
+
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
